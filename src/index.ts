7eab6ee26e1c8bb8e16f6023735e3eaabf0e07aa
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-30 15:16:57 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+
+
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 4590;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (Req: Request, Res: Response) => {
+
+    Res.status(200).json(
+        {
+            email: "Josepholuolofinte@gmail.com",
+            current_datetime: new Date().toISOString(),
+            github_url: "coming soon",
+        }
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
+
