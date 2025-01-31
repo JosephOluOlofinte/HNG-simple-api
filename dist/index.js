12ca00d7f9d5b82ae95f300be1dab415b5db13e0
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-31 10:15:15 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -14,10 +14,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const compression_1 = __importDefault(require("compression"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4590;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use((0, compression_1.default)());
 app.get("/", (Req, Res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         Res.status(200).json({
